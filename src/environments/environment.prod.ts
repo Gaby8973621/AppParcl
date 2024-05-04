2ba857971a8eef957a21cbc6e8655530f9d0fb6b
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2024-05-03 18:22:19 -0600

    Sea agrega en el proyecto el login principal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA2jBpHNkXx51DegsqQG0Ia3ctiPOIDul0",
+    authDomain: "parcial-admin-app.firebaseapp.com",
+    projectId: "parcial-admin-app",
+    storageBucket: "parcial-admin-app.appspot.com",
+    messagingSenderId: "706982830106",
+    appId: "1:706982830106:web:64aadfe16bb328da9af1a3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA2jBpHNkXx51DegsqQG0Ia3ctiPOIDul0",
+    authDomain: "parcial-admin-app.firebaseapp.com",
+    projectId: "parcial-admin-app",
+    storageBucket: "parcial-admin-app.appspot.com",
+    messagingSenderId: "706982830106",
+    appId: "1:706982830106:web:64aadfe16bb328da9af1a3"
+  }
 };
 
 /*
